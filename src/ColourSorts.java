76dc3bb0df7e6e112ccc96c84ee7a0abf55d90f5
Tom Howarth <tom.howarth@hotmail.com>
2017-11-22 18:00:29 +0000

    Insertion Sort
    Implemented insertionSort(), removed Output.png

--- src/ColourSorts.java
+++ src/ColourSorts.java
@@ -45,6 +45,22 @@ public class ColourSorts {
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		insertionSort(array);
+		
+	}
+	
+	private static void insertionSort(int[] input){
+		int temp;
+        for (int i = 1; i < input.length; i++) {
+            for(int j = i ; j > 0 ; j--){
+                if(input[j] < input[j-1]){
+                    temp = input[j];
+                    input[j] = input[j-1];
+                    input[j-1] = temp;
+                    printImage(input);
+                }
+            }
+        }
 	}
 	
 	private static void printImage(int[] array){
